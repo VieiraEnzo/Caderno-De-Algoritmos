384a2319d613e1c42795752ba7be197f394d7b68
VieiraEnzo <vieira.enzof@gmail.com>
2025-12-09 14:35:12 -0300

    New aditions

--- Algoritmos/DataStructures/LazySegTree.h
+++ Algoritmos/DataStructures/LazySegTree.h
@@ -1,104 +1,91 @@
-//SegTree c/ Lazy
+//Lazy SegTree
 
 struct SegTree{ 
-  int n;
-  struct Node{
-    int val;
-    Node operator+(Node other) const{
-      return {this->val + other.val};
+    int n;
+    struct Node{
+        int val;
+        Node operator+(Node other) const{
+        return {this->val + other.val};
+        }
+        Node operator=(int x){
+        return {this->val = x};
+        }
+    };
+    Node neutral = {0};
+    vector <Node> t;
+    vector<int> lazy; //Change variable
+
+    SegTree(vector <int> a){
+        n = a.size();
+        t.resize(4*n);
+        lazy.resize(4*n); //Neutal element
+        build(a, 1, 0, n-1);
     }
-    Node operator=(int x){
-      return {this->val = x};
+
+    void build(vector <int>& a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) / 2;
+            build(a, v*2, tl, tm);
+            build(a, v*2+1, tm+1, tr);
+            t[v] = t[v*2] + t[v*2+1];
+        }
     }
-  };
-  Node neutral = {0};
-  vector <Node> t;
-  vector<int> lazy;
 
-  SegTree(vector <int> a){
-    n = a.size();
-    t.resize(4*n);
-    lazy.resize(4*n);
-    build(a, 1, 0, n-1);
-  }
+    void unlazy(int v, int tl, int tr){
+        // if(lazy[v] == 0) return; //Neutral element
 
-  void build(vector <int>& a, int v, int tl, int tr) {
-    if (tl == tr) {
-      t[v] = a[tl];
-    } else {
-      int tm = (tl + tr) / 2;
-      build(a, v*2, tl, tm);
-      build(a, v*2+1, tm+1, tr);
-      t[v] = t[v*2] + t[v*2+1];
+        //Update current range
+        t[v].val += (tr-tl+1) * lazy[v]; 
+
+        if(tl != tr){
+            lazy[2*v] += lazy[v];   
+            lazy[2*v+1] += lazy[v]; 
+        }
+        
+        lazy[v] = 0; 
     }
-  }
 
-  void unlazy(int v, int tl, int tr){
-    if(lazy[v] == 0) return;
-
-    //Update current range
-    t[v].val += (tr-tl+1) * lazy[v]; 
-
-    //Pass lazy to child if any
-    if(tl != tr){
-        lazy[2*v] += lazy[v];   
-        lazy[2*v+1] += lazy[v]; 
+    Node query(int l, int r){
+        return query(1, 0, n-1, l, r);
     }
-    
-    //Reset lazy
-    lazy[v] = 0; 
-  }
 
-  Node query(int l, int r){
-    return query(1, 0, n-1, l, r);
-  }
-
-  Node query(int v, int tl, int tr, int l, int r){
-    unlazy(v, tl, tr);
-    if (l > r) 
-      return neutral;
-    if (l == tl && r == tr) {
-      return t[v];
-    }
-    int tm = (tl + tr) / 2;
-    return query(v*2, tl, tm, l, min(r, tm))
-      + query(v*2+1, tm+1, tr, max(l, tm+1), r);
-  }
-
-  void RangeUpdate(int l, int r, int new_val){
-    RangeUpdate(1,0,n-1,l,r,new_val);
-  }
-
-  void RangeUpdate(int v, int tl, int tr, int l, int r, int new_val){
-    unlazy(v, tl, tr);
-    if (l > r) 
-      return;
-    if (l == tl && r == tr) {
-        lazy[v] += new_val; //Change here
+    Node query(int v, int tl, int tr, int l, int r){
         unlazy(v, tl, tr);
-        return;
+        if (l > r){
+            return neutral;
+        }
+        if (l == tl && r == tr) {
+            return t[v];
+        }
+        int tm = (tl + tr) / 2;
+        return query(v*2, tl, tm, l, min(r, tm))
+             + query(v*2+1, tm+1, tr, max(l, tm+1), r);
     }
-    int tm = (tl + tr) / 2;
-    RangeUpdate(v*2, tl, tm, l, min(r, tm), new_val);
-    RangeUpdate(v*2+1, tm+1, tr, max(l, tm+1), r, new_val);
-    t[v] = t[2*v] + t[2*v+1];
-  }
 
-  void PointUpdate(int pos, int val){
-    PointUpdate(1, 0, n-1, pos, val);
-  }
-
-  void PointUpdate(int v, int tl, int tr, int pos, int new_val){
-    unlazy(v, tl, tr);
-    if (tl == tr) {
-      t[v] = new_val;
-    } else {
-      int tm = (tl + tr) / 2;
-      if (pos <= tm)
-        PointUpdate(v*2, tl, tm, pos, new_val);
-      else
-        PointUpdate(v*2+1, tm+1, tr, pos, new_val);
-      t[v] = t[v*2] + t[v*2+1];
+    void update(int pos, int val){
+        update(1, 0, n-1, pos, pos, val);
     }
-  }
+
+    void update(int l, int r, int val){
+        update(1, 0, n-1, l, r, val);
+    }
+
+    void update(int v, int tl, int tr, int l, int r, int val){
+        unlazy(v, tl, tr);
+        if (l > r){
+            return;
+        }
+        if (l == tl && r == tr) {
+            lazy[v] += val; //Change here
+            unlazy(v, tl, tr);
+            return;
+        }
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), val);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, val);
+        t[v] = t[2*v] + t[2*v+1];
+    }
+
 };
---
+++ Algoritmos/Grafos/Games.h
@@ -0,0 +1,28 @@
+//Games on arbitrary graphs
+
+vector<vector<int>> adj_rev;
+
+vector<bool> winning;
+vector<bool> losing;
+vector<bool> visited;
+vector<int> degree;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj_rev[v]) {
+        if (!visited[u]) {
+            if (losing[v])
+                winning[u] = true;
+            else if (--degree[u] == 0)
+                losing[u] = true;
+            else
+                continue;
+            dfs(u);
+        }
+    }
+}
+
+for (int i = 0; i < n; i++) {
+    if ((winning[i] || losing[i]) && !visited[i])
+        dfs(i);
+}
